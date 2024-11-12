9984f157bc07f337b00e7de90650398891a9db20
danielaguiar0006 <danielaguiar0006@gmail.com>
2024-11-12 00:22:10 -0800

    Implement a fully (I think) functioning version of AsciiTable's GetAsciiTable() method
    - It correctly produces the correct output when running it through all
    the tests in the AsciiTable.zig file

--- src/AsciiTable.zig
+++ src/AsciiTable.zig
@@ -2,34 +2,35 @@ const std = @import("std");
 
 const Self = @This();
 
-// TODO: Create a list of lists of strings
-// outer list represents every row and column
-// inner list each represent a row themselves and each index represents a column
+/// Each index holds a usize value that represents the width of the column in char lengths
+/// NOTE: Make sure to check and always make sure the array.len is the same as columns_count
+columns_widths: []usize = undefined,
 
-allocator: std.mem.Allocator = undefined,
+/// Outer list represents every row and column
+/// Inner list each represent a row themselves and each index represents a column
 lists: std.ArrayList([][]const u8) = undefined,
-
-/// ! DO NOT MODIFY DIRECTLY
-columns_count: usize = undefined,
-
-/// ! DO NOT MODIFY DIRECTLY
-// TODO: each index holds a usize that represents the width of the column in char lengths
-// Make sure to check and always make sure its length is the same as columns_count
-//columns_widths: []usize = undefined,
+allocator: std.mem.Allocator = undefined,
 
 // Chars that make-up the table borders
 corner_char: u8 = '+',
 horizontal_char: u8 = '-',
 vertical_char: u8 = '|',
-// Probably not needed (instead return through methods):
-// rows_count: usize = undefined,
-// columns_count: usize = undefined,
 
-pub fn init(allocator: std.mem.Allocator, column_count: usize) Self {
+// TODO: column count could probably be calculated from the column widths
+/// ! DO NOT MODIFY DIRECTLY
+columns_count: usize = undefined,
+
+pub fn init(allocator: std.mem.Allocator, column_count: usize, column_widths: []usize) !Self {
+    if (column_widths.len != column_count) {
+        std.debug.print("ERROR: Column widths length does not match columns count\n", .{});
+        return error.InvalidColumnWidthsLength;
+    }
+
     return .{
         .allocator = allocator,
         .lists = std.ArrayList([][]const u8).init(allocator),
         .columns_count = column_count,
+        .columns_widths = column_widths,
     };
 }
 
@@ -37,12 +38,14 @@ pub fn deinit(self: *Self) void {
     self.lists.deinit();
 }
 
-// NOTE: to add a row it must be done in a similar manner as the following:
-// var table_entry = [_][]const u8{ "a", "b" };
-// try ascii_table.addRow(&table_entry);
+/// NOTE: to add a row it must be done in a similar manner as the following:
+/// var table_entry = \[_\]\[\]const u8{ "a", "b" };
+/// try ascii_table.addRow(&table_entry);
 pub fn addRow(self: *Self, row: [][]const u8) !void {
     if (row.len != self.columns_count) {
-        std.debug.print("ERROR: Row length does not match columns count\n", .{});
+        // Commmented out for being to noisy when testing
+        // std.debug.print("ERROR: Row length does not match columns count\n", .{});
+
         return error.InvalidRowLength;
     }
 
@@ -50,29 +53,57 @@ pub fn addRow(self: *Self, row: [][]const u8) !void {
 }
 
 // TODO: eventually seperate out into two methods: GetAsciiTableBuff() and GetAsciiTableAlloc()
-/// Both writes to the buffer or returns a slice of the exact length written (the exact length of the ascii table returned)
-pub fn GetAsciiTable(self: *Self, buffer: []u8) []const u8 {
-    var ascii_table_stream = std.io.fixedBufferStream(buffer);
+/// Both writes to the buffer and returns a slice of the exact length written (the exact length of the ascii table returned)
+pub fn GetAsciiTable(self: *Self, ascii_buffer: []u8) []const u8 {
+    var ascii_table_stream = std.io.fixedBufferStream(ascii_buffer);
     const ascii_table_writer = ascii_table_stream.writer();
+
+    // Repeated_chars represent the top and bottom horizontal table borders
+    var repeated_chars: [1024]u8 = undefined; // HACK: For really large table widths this will be a problem, but it's fine for now...
+    var repeated_chars_stream = std.io.fixedBufferStream(&repeated_chars);
+    const repeated_chars_writer = repeated_chars_stream.writer();
+
+    // Get the repeated chars
+    repeated_chars_writer.print("{c}", .{self.corner_char}) catch unreachable;
+    for (0..self.columns_count) |i| {
+        repeated_chars_writer.print(" ", .{}) catch unreachable;
+        repeated_chars_writer.writeByteNTimes(self.horizontal_char, self.columns_widths[i]) catch unreachable;
+        repeated_chars_writer.print(" {c}", .{self.corner_char}) catch unreachable;
+    }
 
     for (0.., self.lists.items) |i, row| {
         for (0.., row) |j, column_value| {
-            std.debug.print("column: {s}\ni: {d}\n", .{ column_value, i });
+            const current_column_width = self.columns_widths[j];
+
+            // Top left corner - Write top border
             if (i == 0 and j == 0) {
-                ascii_table_writer.print("{c} {c}{c}{c} {c} ", .{ self.corner_char, self.horizontal_char, self.horizontal_char, self.horizontal_char, self.corner_char }) catch unreachable;
-                ascii_table_writer.print("{c}{c}{c} {c}\n", .{ self.horizontal_char, self.horizontal_char, self.horizontal_char, self.corner_char }) catch unreachable;
+                ascii_table_writer.print("{s}", .{repeated_chars_stream.getWritten()}) catch unreachable;
             }
 
-            ascii_table_writer.print("{c}  {s}  ", .{ self.vertical_char, column_value }) catch unreachable;
-        }
+            // First column of a row - Create a new line for the next row
+            if (j == 0) {
+                ascii_table_writer.print("\n{c}", .{self.vertical_char}) catch unreachable;
+            }
 
-        ascii_table_writer.print("{c}\n", .{self.vertical_char}) catch unreachable;
+            // Write the column value into the table
+            if (column_value.len > current_column_width) {
+                ascii_table_writer.print(" {s} {c}", .{ column_value[0..current_column_width], self.vertical_char }) catch unreachable;
+            } else {
+                const padding_amount = current_column_width - column_value.len;
+                ascii_table_writer.print(" {s} ", .{column_value}) catch unreachable;
+                ascii_table_writer.writeByteNTimes(' ', padding_amount) catch unreachable;
+                ascii_table_writer.print("{c}", .{self.vertical_char}) catch unreachable;
+            }
+
+            // Bottom right corner - Write bottom border
+            if (i == self.lists.items.len - 1 and j == self.columns_count - 1) {
+                ascii_table_writer.print("\n{s}", .{repeated_chars_stream.getWritten()}) catch unreachable;
+            }
+        }
     }
-    ascii_table_writer.print("{c} {c}{c}{c} {c} ", .{ self.corner_char, self.horizontal_char, self.horizontal_char, self.horizontal_char, self.corner_char }) catch unreachable;
-    ascii_table_writer.print("{c}{c}{c} {c}", .{ self.horizontal_char, self.horizontal_char, self.horizontal_char, self.corner_char }) catch unreachable;
 
     // Return a slice of the buffer that was written to
-    return buffer[0..ascii_table_stream.pos];
+    return ascii_buffer[0..ascii_table_stream.pos];
 }
 
 // TODO: pub fn GetAsciiTableBuff(self: *Self, buffer: []u8) []const u8 {}
@@ -82,7 +113,9 @@ pub fn GetAsciiTable(self: *Self, buffer: []u8) []const u8 {
 
 test "Adding rows to the table" {
     const allocator = std.testing.allocator;
-    var table = init(allocator, 2);
+
+    var table_widths = [_]usize{ 3, 3 };
+    var table = try init(allocator, 2, &table_widths);
     defer table.deinit();
 
     var row1 = [_][]const u8{ "a", "b" };
@@ -94,16 +127,18 @@ test "Adding rows to the table" {
     try table.addRow(&row2); // OK
     try std.testing.expectError(error.InvalidRowLength, table.addRow(&row3)); // ERROR
     try std.testing.expectError(error.InvalidRowLength, table.addRow(&row4)); // ERROR
-
-    std.debug.print("Table:\n{s}\n", .{table.lists.items});
 }
 
 test "Memory scope" {
     const allocator = std.testing.allocator;
-    var table = init(allocator, 2);
+
+    var table_widths = [_]usize{ 3, 3 };
+    var table = try init(allocator, 2, &table_widths);
     defer table.deinit();
 
-    {
+    for (0..1) |i| {
+        _ = i;
+
         var row1 = [_][]const u8{ "a", "b" };
         try table.addRow(&row1); // OK
     }
@@ -112,9 +147,11 @@ test "Memory scope" {
     try std.testing.expectEqualStrings("b", table.lists.items[0][1]);
 }
 
-test "GetAsciiTable" {
+test "GetAsciiTable - Small column values" {
     const allocator = std.testing.allocator;
-    var table = init(allocator, 2);
+
+    var table_widths = [_]usize{ 3, 3 };
+    var table = try init(allocator, 2, &table_widths);
     defer table.deinit();
 
     var row1 = [_][]const u8{ "a", "b" };
@@ -125,45 +162,39 @@ test "GetAsciiTable" {
 
     const expected_table =
         \\+ --- + --- +
-        \\|  a  |  b  |
-        \\|  c  |  d  |
+        \\| a   | b   |
+        \\| c   | d   |
         \\+ --- + --- +
     ;
 
     var buffer: [2048]u8 = undefined;
     const generated_table = table.GetAsciiTable(&buffer);
-    std.debug.print("Generated Table:\n{s}\n", .{generated_table});
 
     try std.testing.expectEqualStrings(expected_table, generated_table);
 }
 
-// TODO:
-// test "Printing ascii table with two columns" {
-//     const allocator = std.testing.allocator;
-//     var table = init(allocator, 2);
-//     defer table.deinit();
-//
-//     var row1 = [_][]const u8{ "a", "b" };
-//     var row2 = [_][]const u8{ "c", "d" };
-//
-//     try table.addRow(&row1); // OK
-//     try table.addRow(&row2); // OK
-//
-//     std.testing.expectEqualStrings("+ ---", table.GetAsciiTable());
-//
-//     std.debug.print("Table:\n{s}\n", .{table.GetAsciiTable()});
-// }
-//
-// test "Printing ascii table with three columns" {
-//     const allocator = std.testing.allocator;
-//     var table = init(allocator, 3);
-//     defer table.deinit();
-//
-//     var row1 = [_][]const u8{ "a", "b", "c" };
-//     var row2 = [_][]const u8{ "d", "e", "f" };
-//
-//     try table.addRow(&row1); // OK
-//     try table.addRow(&row2); // OK
-//
-//     std.debug.print("Table:\n{s}\n", .{table.GetAsciiTable()});
-// }
+test "GetAsciiTable - large/overflowing column values" {
+    const allocator = std.testing.allocator;
+
+    var table_widths = [_]usize{ 5, 3, 10, 1 };
+    var table = try init(allocator, 4, &table_widths);
+    defer table.deinit();
+
+    var row1 = [_][]const u8{ "Daniel", "Aguiar", "yo-reign", "yo" };
+    var row2 = [_][]const u8{ "a", "-", " ", "" };
+
+    try table.addRow(&row1);
+    try table.addRow(&row2);
+
+    const expected_table =
+        \\+ ----- + --- + ---------- + - +
+        \\| Danie | Agu | yo-reign   | y |
+        \\| a     | -   |            |   |
+        \\+ ----- + --- + ---------- + - +
+    ;
+
+    var buffer: [2048]u8 = undefined;
+    const generated_table = table.GetAsciiTable(&buffer);
+
+    try std.testing.expectEqualStrings(expected_table, generated_table);
+}
